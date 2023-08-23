fd8a0f6a4da97296617c49243718d8ab320ed633
segni-tafesse <segnifoka2023@gmail.com>
2023-08-23 10:04:26 +0300

    remove the main funciton in onepag

---
+++ lib/details_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DetailsScreen extends StatelessWidget {
+  const DetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("this is the details screen"),
+      ),
+    );
+  }
+}
--- lib/onepage.dart
+++ lib/onepage.dart
@@ -1,10 +1,6 @@
 import 'package:ethio_agri_smart/model/crop_model.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
