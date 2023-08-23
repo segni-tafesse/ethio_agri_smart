b2d3ef7b55104be82b7a5d5d25d2d2b538e184e4
segni-tafesse <segnifoka2023@gmail.com>
2023-08-23 14:03:06 +0300

    search functionality works

--- lib/details_screen.dart
+++ lib/details_screen.dart
@@ -6,12 +6,15 @@ class DetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Center(
-            child: Text("this is the details screen"),
-          ),
-        ],
+      appBar: AppBar(
+        title: Text('Apple'),
+        centerTitle: true,
+        backgroundColor: Colors.red[400],
+      ),
+      body: Center(
+        child: Image(
+            image: NetworkImage(
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSj3WdD-zEumIC95q3rBp4IubqIIIn5Lt0VlA&usqp=CAU')),
       ),
     );
   }
